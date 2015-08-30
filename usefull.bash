c37b48776d94275945e7d562d0fb1155830c6b00
juno <juno@krasava.fi>
2015-08-30 03:30:54 +0300

    prod

--- usefull.bash
+++ usefull.bash
@@ -1,3 +1,6 @@
 git add version_14 && git commit -m "Initial dist subtree commit"
 
 git subtree push --prefix version_14  origin version_14
+
+git add version_17 && git commit -m "Initial dist subtree commit"
+git subtree push --prefix version_17  origin version_17

103ffd850f4fab3471ef0e381001b7128ada2e54
juno <juno@krasava.fi>
2015-12-08 05:23:16 +0200

    Initial dist subtree commit

---
+++ version_23/src/app/filters/filters.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc filter
+ * @name oyblogApp.filter:filters
+ * @function
+ * @description
+ * # filters
+ * Filter in the oyblogApp.
+ */
+angular.module('FiFiDeskMobile')
+  .filter('img_link', function () {
+    return function (input) {
+
+        return "/img/";     
+                 
+    };
+  }).filter('perm_link', function () {
+	    return function (input) {
+	    	
+	    	return input.replace(' ','-')+'.html';	        
+	    }
+	                 
+  });
+
+  
